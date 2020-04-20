4a2aaa293e9670b5c593b83ed9364c7af3581c98
heckenna <heckenna@rose-hulman.edu>
2020-04-19 21:01:26 -0400

    End of meeting 2.

--- NRCemotionParser.py
+++ NRCemotionParser.py
@@ -1,8 +1,14 @@
 import pandas as pd
 
-myCsv = pd.read_csv("NRC-Emotion-Lexicon-v0.92\\NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.csv", sep = '|' ,usecols = ["English (en)", "Positive", "Negative", "Anger", "Anticipation", "Disgust", "Fear", "Joy", "Sadness", "Surprise", "Trust"])
 
-print(myCsv)
+
+def getNRCemotions():
+  myCsv = pd.read_csv("NRC-Emotion-Lexicon-v0.92\\NRC-Emotion-Lexicon-v0.92-In105Languages-Nov2017Translations.csv", sep = '|' ,usecols = ["English (en)", "Positive", "Negative", "Anger", "Anticipation", "Disgust", "Fear", "Joy", "Sadness", "Surprise", "Trust"])
+  myCsv = myCsv.rename(columns={"English (en)":"Word"})
+  return myCsv
+
+
+#print(getNRCemotions())
 
 
 
--- sloganHtmlExtractor.py
+++ sloganHtmlExtractor.py
@@ -1,3 +1,85 @@
-from html.parser import HTMLParser
+from lxml import html
+import requests
+import pprint
+import re
+import json
+import time
 
-print("HELLO")
+base_url = "http://www.textart.ru/"
+
+def parseSloganPage(url):
+
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    def removeEmsp(x): return x.replace("\u2003", "")
+    def startsNewline(x): return not x[0] == "\n"
+    def removeNonAlpha(x): return re.sub('[\W_]+', ' ', x, flags=re.UNICODE)
+    # print(list(map(removeEmsp, tree.xpath('//p[@class="paragraf"]/text()'))))
+    companies = list(map(removeNonAlpha, list(filter(startsNewline, list(
+        map(removeEmsp, tree.xpath('//p[@class="paragraf"]/text()')))))))
+    slogans = list(map(removeEmsp, tree.xpath(
+        '//span[@class="slogan"]/text()')))
+    pairs = dict()
+    print(url)
+    # print(companies, len(slogans))
+    j = 0
+    for i, slogan in enumerate(slogans):
+
+        if not "\n" in slogan:
+            if(not companies[j] in pairs):
+                pairs[companies[j]] = list()
+            # print(companies[j], ">>", slogan)
+            pairs[companies[j]].append(slogan)
+            j += 1
+        else:
+            slogan = re.sub('[\W_]+', ' ', slogan, flags=re.UNICODE)
+
+            pairs[companies[j - 1]].append(slogan)
+            # print(companies[j - 1], ">>", slogan)
+
+    return pairs
+
+def formatUrl(rawUrl, categoryUrl):
+    if not base_url in rawUrl:
+        url = categoryUrl.replace("index.html", rawUrl)
+        # print(url)
+        return url
+    return rawUrl
+
+def parseCategories():
+
+    links = open('SloganLinks.json')
+    data = json.load(links)
+    categories = dict()
+    for category in data:
+        categories[category] = dict()
+
+        page = requests.get(data[category])
+        tree = html.fromstring(page.content)
+        subcategories = tree.xpath('//option[@value]/text()')
+        subcategories_links = tree.xpath('//option[@value]//@value')
+        for i in range(len(subcategories)):
+            if(subcategories_links[i] == "#" or subcategories[i] == "Alcoholic drinks"):
+                continue
+            
+            subDict = parseSloganPage(formatUrl(subcategories_links[i], data[category]))
+            categories[category][subcategories[i]] = subDict
+            time.sleep(0.05)
+        time.sleep(0.1)
+        #pprint.pprint(categories[category])
+
+    output = open('rawSlogans.json', 'w')
+    json.dump(categories, output)
+    output.close()
+    links.close()
+
+    f = open("rawSlogans.json", "a")
+    f.write("")
+    f.close()
+    # print(categories)
+
+
+parseCategories()
+page_url = 'http://www.textart.ru/database/english-advertising-slogans/restaurant-advertising-slogans.html'
+# print(parseSloganPage(page_url))
+
